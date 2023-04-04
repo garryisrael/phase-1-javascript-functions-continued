9b55e4d80b3cf544912ad786e17bbe2ee42cdf53
Garry <garryisrael02@gmail.com>
2023-04-04 07:27:50 -0400

    Codes function-level scope

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(string="roller-skate"){
+  return "This Saturday, I want to "+string+"!";
+}
+
+let mondayWork = function(activity="go to the office"){
+  return "This Monday, I will "+activity+".";
+}
+
+function wrapAdjective(expression){
+  const innerfunction = function(message="special"){
+     return `You are ${expression}${message}${expression}!`
+  }
+    return innerfunction;
+}
